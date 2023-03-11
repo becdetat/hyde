b2f70c632ecf9bdd2c4974a982111e0cfd92ed8b
Rebecca Scott <me@becdetat.com>
2023-03-12 07:01:45 +1000

    It's full of XAML!

---
+++ src/Hyde/Hyde/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Hyde;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Hyde/Hyde/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Hyde;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
